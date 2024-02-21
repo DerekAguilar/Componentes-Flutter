8899d745f5a3609dc1ba06843d40eb4c74bc98ef
Derek Eduardo Aguilar Reyes <aguilarrde753@gmail.com>
2024-02-21 11:01:05 -0600

    Navigator.push to ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/screens/infinite_scroll_screen.dart';
+import 'package:practica3/screens/inputs_screen.dart';
+import 'package:practica3/screens/notifications_screen.dart';
 import 'package:practica3/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -19,6 +22,12 @@ class HomeScreen extends StatelessWidget {
             style: AppTheme.lightTheme.textTheme.bodySmall),
             leading: const Icon(Icons.input_rounded),
             trailing: const Icon(Icons.keyboard_arrow_right),
+            onTap:() {
+              final ruta1=MaterialPageRoute(builder: (context) {
+                return const InputsScreen();
+              });
+              Navigator.push(context, ruta1);
+            },
           ),
           const Divider(),
           ListTile(
@@ -28,6 +37,12 @@ class HomeScreen extends StatelessWidget {
             style: AppTheme.lightTheme.textTheme.bodySmall,),
             leading: const Icon(Icons.list_alt_rounded),
             trailing: const Icon(Icons.keyboard_arrow_right),
+            onTap:() {
+              final ruta1=MaterialPageRoute(builder: (context) {
+                return const InfiniteScrollScreen();
+              });
+              Navigator.push(context, ruta1);
+            },
           ),
           const Divider(),
           ListTile(
@@ -37,6 +52,12 @@ class HomeScreen extends StatelessWidget {
             style: AppTheme.lightTheme.textTheme.bodySmall,),
             leading: const Icon(Icons.notifications_outlined),
             trailing: const Icon(Icons.keyboard_arrow_right),
+            onTap:() {
+              final ruta1=MaterialPageRoute(builder: (context) {
+                return const NotificationScreen();
+              });
+              Navigator.push(context, ruta1);
+            },
           ),
         ],
       )
